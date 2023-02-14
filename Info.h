071cf710a483c9adefd93fcd31f48ee180783db6
SubveYninja <1metropolit1@gmail.com>
2023-02-14 22:30:37 +0300

    Добавьте файлы проекта.

---
+++ Info.h
@@ -0,0 +1,55 @@
+#pragma once
+
+struct
+{
+	int iIndex;
+	const wchar_t* szLabel;
+	const wchar_t* szDesc;
+} sysmetrics[] =
+{
+		SM_CXSCREEN, L"SM_CXSCREEN", L"Screen width in pixels",
+		SM_CYSCREEN, L"SM_CYSCREEN", L"Screen height in pixels",
+		SM_CXVSCROLL, L"SM_CXVSCROLL", L"Vertical scroll arrow width",
+		SM_CYHSCROLL, L"SM_CYHSCROLL", L"Horizontal scroll arrow height",
+		SM_CYCAPTION, L"SM_CYCAPTION", L"Caption bar height",
+		SM_CXBORDER, L"SM_CXBORDER", L"Window border width",
+		SM_CYBORDER, L"SM_CYBORDER", L"Window border height",
+		SM_CXDLGFRAME, L"SM_CXDLGFRAME", L"Dialog window frame width",
+		SM_CYDLGFRAME, L"SM_CYDLGFRAME", L"Dialog window frame height",
+		SM_CYVTHUMB, L"SM_CYVTHUMB", L"Vertical scroll thumb height",
+		SM_CXHTHUMB, L"SM_CXHTHUMB", L"Horizontal scroll thumb width",
+		SM_CXICON, L"SM_CXICON", L"Icon width",
+		SM_CYICON, L"SM_CYICON", L"Icon height",
+		SM_CXCURSOR, L"SM_CXCURSOR", L"Cursor width",
+		SM_CYCURSOR, L"SM_CYCURSOR", L"Cursor height",
+		SM_CYMENU, L"SM_CYMENU", L"Menu bar height",
+		SM_CXFULLSCREEN, L"SM_CXFULLSCREEN", L"Full screen client area width",
+		SM_CYFULLSCREEN, L"SM_CYFULLSCREEN", L"Full screen client area height",
+		SM_CYKANJIWINDOW, L"SM_CYKANJIWINDOW", L"Kanji window height",
+		SM_MOUSEPRESENT, L"SM_MOUSEPRESENT", L"Mouse present flag",
+		SM_CYVSCROLL, L"SM_CYVSCROLL", L"Vertical scroll arrow height",
+		SM_CXHSCROLL, L"SM_CXHSCROLL", L"Horizontal scroll arrow width",
+		SM_DEBUG, L"SM_DEBUG", L"Debug version flag",
+		SM_SWAPBUTTON, L"SM_SWAPBUTTON", L"Mouse buttons swapped flag",
+		SM_RESERVED1, L"SM_RESERVED1", L"Reserved",
+		SM_RESERVED2, L"SM_RESERVED2", L"Reserved",
+		SM_RESERVED3, L"SM_RESERVED3", L"Reserved",
+		SM_RESERVED4, L"SM_RESERVED4", L"Reserved",
+		SM_CXMIN, L"SM_CXMIN", L"Minimum window width",
+		SM_CYMIN, L"SM_CYMIN", L"Minimum window height",
+		SM_CXSIZE, L"SM_CXSIZE", L"Minimize/Maximize icon width",
+		SM_CYSIZE, L"SM_CYSIZE", L"Minimize/Maximize icon height",
+		SM_CXFRAME, L"SM_CXFRAME", L"Window frame width",
+		SM_CYFRAME, L"SM_CYFRAME", L"Window frame height",
+		SM_CXMINTRACK, L"SM_CXMINTRACK", L"Minimum window tracking width",
+		SM_CYMINTRACK, L"SM_CYMINTRACK", L"Minimum window tracking height",
+		SM_CXDOUBLECLK, L"SM_CXDOUBLECLK", L"Double click x tolerance",
+		SM_CYDOUBLECLK, L"SM_CYDOUBLECLK", L"Double click y tolerance",
+		SM_CXICONSPACING, L"SM_CXICONSPACING", L"Horizontal icon spacing",
+		SM_CYICONSPACING, L"SM_CYICONSPACING", L"Vertical icon spacing",
+		SM_MENUDROPALIGNMENT, L"SM_MENUDROPALIGNENT", L"Left or right menu drop",
+		SM_PENWINDOWS, L"SM_PENWINDOWS", L"Pen extensions installed",
+		SM_DBCSENABLED, L"SM_DBCSENABLED",L"Double-Byte Char Set enabled",
+		SM_CMOUSEBUTTONS, L"SM_CMOUSEBUTTONS", L"Number of mouse buttons",
+		SM_SHOWSOUNDS, L"SM_SHOWSOUNDS", L"Present sounds visually"
+};
---
+++ Main.cpp
@@ -0,0 +1,260 @@
+﻿#include <Windows.h>
+#include "Info.h"
+
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+const int lines = sizeof(sysmetrics) / sizeof(sysmetrics[0]);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
+{
+	static char szAppName[] = "HelloWin";
+	HWND hwnd;
+	MSG msg;
+	WNDCLASSEX wndclass{};
+
+	wndclass.cbSize = sizeof(wndclass);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndclass.lpszMenuName = NULL;
+	wndclass.lpszClassName = (LPCWSTR)szAppName;
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	RegisterClassEx(&wndclass);
+
+	hwnd = CreateWindow(
+		(LPCWSTR)szAppName, // window class name 
+		L"The Hello Program", // window caption 
+		WS_OVERLAPPEDWINDOW | WS_VSCROLL, // window style 
+		CW_USEDEFAULT, // initial x position 
+		CW_USEDEFAULT, // initial y position 
+		CW_USEDEFAULT, // initial x size 
+		CW_USEDEFAULT, // initial y size 
+		NULL, // parent window handle 
+		NULL, // window menu handle 
+		hInstance, // program instance handle 
+		NULL
+	); // creation parameters 
+
+	ShowWindow(hwnd, iCmdShow);
+	UpdateWindow(hwnd);
+
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return msg.wParam;
+}
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	PAINTSTRUCT ps;
+	//RECT rect;
+	TEXTMETRIC tm{};
+	HMENU hMenu;
+
+	static int cxChar, cyChar, cxCaps;
+	static int cyClient, cxClient, iMaxWidth;
+	static int iVscrollPos, iVscrollMax, iHscrollPos, iHscrollMax;
+
+	wchar_t szBuffer[10]{};
+
+	int iVscrollInc, iHscrollInc;
+	int iPaintBeg, iPaintEnd;
+
+	switch (iMsg)
+	{
+	//case WM_COMMAND:
+
+	//	hMenu = GetMenu(hwnd);
+
+	//	switch (LOWORD(wParam))
+	//	{
+	//	case :
+
+
+
+	//		break;
+	//	default:
+	//		break;
+	//	}
+
+	//	break;
+
+	case WM_CREATE:
+
+		hdc = BeginPaint(hwnd, &ps);
+
+		GetTextMetrics(hdc, &tm);
+		cxChar = tm.tmAveCharWidth;
+		cxCaps = (tm.tmPitchAndFamily & 1 ? 3 : 2) * cxChar / 2;
+		cyChar = tm.tmHeight + tm.tmExternalLeading;
+
+		EndPaint(hwnd, &ps);
+
+		iMaxWidth = 40 * cxCaps + 22 * cxCaps;
+
+		return 0;
+
+	case WM_SIZE:
+
+		cyClient = HIWORD(lParam);
+		cxClient = HIWORD(lParam);
+
+		iVscrollMax = max(0, lines + 2 - cyClient / cyChar);
+		iVscrollPos = min(iVscrollPos, iVscrollMax);
+
+		SetScrollRange(hwnd, SB_VERT, 0, iVscrollMax, FALSE);
+		SetScrollPos(hwnd, SB_VERT, iVscrollPos, TRUE);
+
+		iHscrollMax = max(0, 2 + (iMaxWidth - cxClient) / cxChar);
+		iHscrollPos = min(iHscrollPos, iHscrollMax);
+
+		SetScrollRange(hwnd, SB_HORZ, 0, iHscrollMax, FALSE);
+		SetScrollPos(hwnd, SB_HORZ, iHscrollPos, TRUE);
+
+		return 0;
+
+	case WM_VSCROLL:
+
+		switch (LOWORD(wParam))
+		{
+		case SB_TOP:
+
+			iVscrollInc = -iVscrollPos;
+			break;
+
+		case SB_BOTTOM:
+
+			iVscrollInc = iVscrollMax - iVscrollPos;
+			break;
+
+		case SB_LINEUP:
+
+			iVscrollInc = -1;
+			break;
+
+		case SB_LINEDOWN:
+
+			iVscrollInc = 1;
+			break;
+
+		case SB_PAGEUP:
+
+			iVscrollInc = min(-1, -cyClient / cyChar);
+			break;
+
+		case SB_PAGEDOWN:
+
+			iVscrollInc = max(1, cyClient / cyChar);
+			break;
+
+		case SB_THUMBTRACK:
+
+			iVscrollInc = HIWORD(wParam) - iVscrollPos;
+			break;
+
+		default:
+
+			iVscrollInc = 0;
+			break;
+		}
+
+		iVscrollInc = max(-iVscrollPos, min(iVscrollInc, iVscrollMax - iVscrollPos));
+
+		if (iVscrollInc != 0)
+		{
+			iVscrollPos += iVscrollInc;
+			ScrollWindow(hwnd, 0, -cyChar * iVscrollInc, NULL, NULL);
+			SetScrollPos(hwnd, SB_VERT, iVscrollPos, TRUE);
+			UpdateWindow(hwnd);
+		}
+
+		return 0;
+
+	case WM_HSCROLL:
+
+		switch (LOWORD(wParam))
+		{
+		case SB_LINEUP:
+
+			iHscrollInc = -1;
+			break;
+
+		case SB_LINEDOWN:
+
+			iHscrollInc = 1;
+			break;
+
+		case SB_PAGEUP:
+
+			iHscrollInc = min(-1, -cxClient / cxChar);
+			break;
+
+		case SB_PAGEDOWN:
+
+			iHscrollInc = max(1, cxClient / cxChar);
+			break;
+
+		case SB_THUMBTRACK:
+
+			iHscrollInc = HIWORD(wParam) - iHscrollPos;
+			break;
+
+		default:
+
+			iHscrollInc = 0;
+			break;
+		}
+
+		iHscrollInc = max(-iHscrollPos, min(iHscrollInc, iHscrollMax - iHscrollPos));
+
+		if (iHscrollInc != 0)
+		{
+			iHscrollPos += iHscrollInc;
+			ScrollWindow(hwnd, -cxChar * iHscrollInc, 0, NULL, NULL);
+			SetScrollPos(hwnd, SB_HORZ, iHscrollPos, TRUE);
+			UpdateWindow(hwnd);
+		}
+
+		return 0;
+
+	case WM_PAINT:
+
+		hdc = BeginPaint(hwnd, &ps);
+
+		iPaintBeg = max(0, iVscrollPos + ps.rcPaint.top / cyChar - 1);
+		iPaintEnd = min(lines, iVscrollPos + ps.rcPaint.bottom / cyChar);
+
+		for (int x, y, i = iPaintBeg; i < iPaintEnd; i++)
+		{
+			x = cxChar * (1 - iHscrollPos);
+			y = cyChar * (1 - iVscrollPos + i);
+
+			TextOut(hdc, x, y, sysmetrics[i].szLabel, lstrlenW(sysmetrics[i].szLabel));
+			TextOut(hdc, x + 22 * cxCaps, y, sysmetrics[i].szDesc, lstrlenW(sysmetrics[i].szDesc));
+			SetTextAlign(hdc, TA_RIGHT | TA_TOP);
+
+			TextOut(hdc, x + 22 * cxCaps + 40 * cxChar, y, szBuffer, wsprintfW(szBuffer, L"%5d", GetSystemMetrics(sysmetrics[i].iIndex)));
+			SetTextAlign(hdc, TA_LEFT | TA_TOP);
+		}
+
+		EndPaint(hwnd, &ps);
+		return 0;
+
+	case WM_DESTROY:
+
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProc(hwnd, iMsg, wParam, lParam);
+}
